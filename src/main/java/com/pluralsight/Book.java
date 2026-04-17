d7f358f81afe4b87039a28637e25d56485d5fcb1
RickelviC <Cruzrickelvi@gmail.com>
2026-04-17 14:36:48 -0400

    refactored Main to Library and old Library to book

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,13 +1,13 @@
 package com.pluralsight;
 
-public class library {
+public class Book {
     private int id;
     private String isbn;
     private String title;
     private boolean isCheckedOut;
     private String checkedOutTo;
 
-    public library(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
