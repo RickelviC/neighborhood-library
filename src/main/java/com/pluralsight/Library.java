769b3ebbaff421c6b7c32a9de9cc21ad4c4ad728
RickelviC <Cruzrickelvi@gmail.com>
2026-04-17 19:31:54 -0400

    lost on how to print the name of the user

--- src/main/java/com/pluralsight/Library.java
+++ src/main/java/com/pluralsight/Library.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Library {
 
     private static Book[] book = new Book[20];
-    private static String name = "";
     private static int numOfBook = 7;
 
     public static void main(String[] args) {
 
-        book[0] = new Book(1, "9780192832696", "the great gatsby", false, name);
-        book[1] = new Book(2, "9780061120084", "To Kill a Mockingbird", false, name);
-        book[2] = new Book(3, "9781937007683", "prince of thorns", false, name);
-        book[3] = new Book(4, "9781839081521", "You Are (Not) Deadpool", false, name);
-        book[4] = new Book(5, "9780062971487", "The Man Who Lived Underground", false, name);
-        book[5] = new Book(6, "9781416936473", "hatchet", false, name);
-        book[6] = new Book(7, "9780261102217", "the hobbit", false, name);
+        book[0] = new Book(1, "9780192832696", "the great gatsby", false, "");
+        book[1] = new Book(2, "9780061120084", "To Kill a Mockingbird", false, "");
+        book[2] = new Book(3, "9781937007683", "prince of thorns", false, "");
+        book[3] = new Book(4, "9781839081521", "You Are (Not) Deadpool", false, "");
+        book[4] = new Book(5, "9780062971487", "The Man Who Lived Underground", false, "");
+        book[5] = new Book(6, "9781416936473", "hatchet", false, "");
+        book[6] = new Book(7, "9780261102217", "the hobbit", false, "");
 
         Scanner scanner = new Scanner(System.in);
+        String name = getName(scanner);
 
         boolean exit = false;
 
@@ -39,6 +39,7 @@ public class Library {
                     pickBook(scanner);
                     break;
                 case 2:
+                    checkedOutBook(scanner);
                     break;
                 case 3:
                     System.out.println("closing out of the application");
@@ -52,6 +53,7 @@ public class Library {
 
     }
 
+    //shows every book
     public static void listAllBooks() {
 
         System.out.println("Books inventory:");
@@ -73,13 +75,31 @@ public class Library {
             System.out.println("going back");
         } else if (userInput <= numOfBook) {
 
-            System.out.println("please enter your name");
-            name = scanner.nextLine();
+
+            String name = getName(scanner);
 
             System.out.println("Thank you " + name + " you choose " + book[userInput - 1].getTitle());
+
+            book[userInput - 1].checkOut(name);
+
         } else {
             System.out.println("Not an option");
         }
+    }
+
+    public static void checkedOutBook(Scanner scanner){
+
+        for (int i = 0; i < numOfBook; i++) {
+            if(book[i].isCheckedOut()){
+                System.out.println(book[i] + " -> " + name);
+            }
+        }
+
+    }
+
+    public static String getName(Scanner scanner){
+        System.out.println("please enter your name");
+        return scanner.nextLine();
     }
 }
 
